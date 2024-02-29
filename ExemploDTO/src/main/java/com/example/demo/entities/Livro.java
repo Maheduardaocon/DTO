d20c30746e68cc60da2eea6f41d30744c08ee7f0
Maria Eduarda <133990702+Maheduardaocon@users.noreply.github.com>
2024-02-29 10:48:37 -0300

    Add files via upload

--- ExemploDTO/src/main/java/com/example/demo/entities/Livro.java
+++ ExemploDTO/src/main/java/com/example/demo/entities/Livro.java
@@ -3,6 +3,7 @@ package com.example.demo.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,14 +14,39 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Livro {
-	
-	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@NotBlank
 	private String titulo;
-	
+
 	@NotBlank
 	private String autor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+
+	}
 
 }
